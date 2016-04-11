a5aece89716609e54226644bccef90143aeac7f3
vohl <acvohl@gmail.com>
2016-04-11 15:09:20 -0500

    Creating the header file for Life

---
+++ Life.h
@@ -0,0 +1,43 @@
+#ifndef LIFE_H
+#define LIFE_H
+
+using namespace std;
+
+//--------------
+// AbstractCell
+//--------------
+class AbstractCell{
+
+};
+
+//This is a handle class.... I dont know what that means
+//------
+// Cell
+//------
+class Cell{
+
+};
+
+//------------
+// ConwayCell
+//------------
+class ConwayCell{
+
+};
+
+//-------------
+// FredkinCell
+//-------------
+class FredkinCell{
+
+};
+
+//------
+// Life
+//------
+Template<Typename T>
+class Life{
+
+};
+
+#endif LIFE_H
