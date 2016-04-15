3d20de8ec08c003a249065d0dc4fbf7f86a704cc
vohl <acvohl@gmail.com>
2016-04-14 23:04:14 -0500

    general structure for the header file done

--- Life.h
+++ Life.h
@@ -1,21 +1,40 @@
 #ifndef LIFE_H
 #define LIFE_H
 
+//----------
+// includes
+//----------
+#include <cstddef>
+#include <iostream>
+#include <vector>
+
 using namespace std;
 
 //--------------
 // AbstractCell
 //--------------
 class AbstractCell{
+protected:
+    char _state;
+    bool _life;
+public:
+    AbstractCell(char state);
 
-};
+    ~AbstractCell();
 
-//This is a handle class.... I dont know what that means
-//------
-// Cell
-//------
-class Cell{
+    //a getter for your life, but techinically just telling
+    //me if you are alive/ I dont necceserily have to maintain
+    //a private member that represent if you are alive
+    bool isAlive();
 
+    //should this cell consider diagonal neighbors??
+    virtual bool countDiagonals() = 0;
+
+    //changing the state of the cell
+    virtual void updateLife(int neighbors) = 0;
+
+    //so we can print it out durring an update
+    friend ostream& operator <<(ostream& lhs, const AbstractCell& rhs);
 };
 
 //inherits from abstract cell
@@ -23,23 +42,108 @@ class Cell{
 // ConwayCell
 //------------
 class ConwayCell : public AbstractCell{
+public:
+    ConwayCell(char state);
 
+    ~ConwayCell();
+
+    bool countDiagonals();
+
+    void updateLife(int neighbors);
+
+    friend ostream& operator <<(ostream& lhs, const ConwayCell& rhs);
 };
 
-//inherits
+//inherits from abstract cell
 //-------------
 // FredkinCell
 //-------------
 class FredkinCell : public AbstractCell{
+private:
+    int _age;
+public:
+    FredkinCell(char state);
 
+    ~FredkinCell();
+
+    bool countDiagonals();
+
+    void updateLife(int neighbors);
+
+    friend ostream& operator <<(ostream& lhs, const FredkinCell& rhs);
+};
+
+//------
+// Cell
+//------
+class Cell{
+private:
+    AbstractCell * _handle_cell;
+public:
+    Cell(char alive);
+
+    Cell(const Cell& rhs);
+
+    Cell& operator =(const Cell& rhs);
+
+    ~Cell();
+
+    AbstractCell * operator ->();
+
+    bool countDiagonals();
+
+    void updateLife(int neighbors);
+
+    friend ostream& operator <<(ostream& lhs, const Cell& rhs);
 };
 
 //------
 // Life
 //------
-Template<Typename T>
+Template<typename T>
 class Life{
+public:
+    using value_type = T;
 
+    using reference = value_type&;
+    using const_reference = const value_type&;
+
+private:
+    int _x;
+    int _y;
+    int _generation;
+    int _population;
+    vector<T> _board;
+    vector<int> _neighbor_board;
+
+    void show(int evolution);
+
+public:
+    Life(int y, int x);
+
+    void readInGrid(istream & r);
+
+    void simulate(int evolution, int step);
+
+    vector<value_type>::iterator begin();
+
+    vector<value_type>::const_iterator begin() const;
+
+    vector<value_type>::iterator end();
+
+    vector<value_type>::const_iterator end() const;
+
+    reference at(size_type);
+
+    const_reference at(size_type) const;
+
+    bool operator == (const Life &rhs);
+
+    bool operator == (const Life &rhs) const;
+
+    bool operator != (const Life &rhs);
+
+    bool operator != (const Life &rhs) const;
 };
 
 #endif LIFE_H
